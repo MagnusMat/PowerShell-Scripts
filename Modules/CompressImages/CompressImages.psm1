c8979fcb09bacf753d057f022354b1fd349eb356
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2022-05-18 17:22:33 +0200

    Added help and default case

--- Modules/CompressImages/CompressImages.psm1
+++ Modules/CompressImages/CompressImages.psm1
@@ -1,18 +1,52 @@
+<#
+ .Synopsis
+  Allows for compressing and converting image files.
+
+ .Description
+  Allows for compressing and converting image files.
+  This uses FFMPEG and supports alomst all image file types.
+
+ .Parameter fileType
+  [String] Target files of this type.
+  Leave blank or use "*" for all file types.
+
+ .Parameter newType
+  [String] Convert to this file type.
+  Leave blank to keep same file type after compression.
+
+ .Parameter appendText
+  [String] Append this text to the end of the file name.
+  Leave blank for no text appended.
+
+ .Example
+   # Convert all .png files in a folder to .webp.
+   CompressImages -fileType ".png" -newType ".webp"
+
+ .Example
+   # Compress all files in a folder.
+   CompressImages -fileType "*"
+
+ .Example
+   # Add comp to the end of all files after compression.
+   CompressImages -fileType "*" -appendText "comp"
+#>
+
 function CompressImages {
-    param([string]$filetype, [string]$newType)
+    param([string]$fileType = "*", [string]$newType, [string]$appendText = "")
 
-    foreach ($i in Get-ChildItem -filter "*.$filetype" $SomeFolder) {
-        $noExtension = ([io.fileinfo]"$i").basename;
+    foreach ($i in Get-ChildItem -filter "*$fileType" $SomeFolder) {
+        $noExtension = ([io.fileinfo]"$i").BaseName;
+        $baseExtension = ([io.fileinfo]"$i").Extension;
 
-        Write-Output $noExtension
-        Write-Output $filetype
-        Write-Output $i
+        if ("$newType" -eq "" -or $null) {
+            $newType = $baseExtension
+        }
 
-        if ("$filetype" -eq "png") {
-            ffmpeg -i "$i" -compression_level 100 "$noExtension comp.png"
+        if ("$fileType" -eq ".png" -or "png") {
+            ffmpeg -i "$i" -compression_level 100 "$noExtension$appendText.png"
         }
         else {
-            ffmpeg -i "$i" "$noExtension comp.$newType"
+            ffmpeg -i "$i" "$noExtension$appendText$newType"
         }
     }
 }
